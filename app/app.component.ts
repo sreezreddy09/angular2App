27c7f6453eb9b79711898111c19ec25e5014f083
Srikanth Reddy Musku <Srikanth Reddy Musku>
2016-06-25 15:56:17 -0500

    add files to repo

---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'top-heading',
+  template: '<h1 class = "info">Top Rated Amenities In and Around [Listing Name]</h1>'
+})
+export class AppComponent { }
